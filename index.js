1d05c5a8c08f6cc6b3e0a4374c1bb7cdb04d9495
Tomasz1982 <64226749+Tomasz1982@users.noreply.github.com>
2024-01-17 21:56:00 +0100

    second commit

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const calculate = (operator, a, b) => {
-    switch (operator) {
-        case '+': return a + b;
-        case '-': return a - b;
-        case '*': return a * b;
-        case '/': return a / b;
-        default: throw new Error('Invalid operator!')
-    }
-}
+  switch (operator) {
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "*":
+      return a * b;
+    case "/":
+      return a / b;
+    default:
+      throw new Error("Invalid operator!");
+  }
+};
 
-console.log(calculate('+', 2, 4));
-console.log(calculate('-', 2, 4));
-console.log(calculate('*', 2, 4));
-console.log(calculate('/', 2, 4));
+console.log(calculate("+", 2, 4));
+console.log(calculate("-", 2, 4));
+console.log(calculate("*", 2, 4));
+console.log(calculate("/", 2, 4));
+console.log("new Branch added");
+// new branch, new Pull request.
