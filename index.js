1525d88702bad48e61ebf9fe6c19bb96a4366962
oohajo <99666208+oohajo@users.noreply.github.com>
2024-01-22 16:18:47 +0100

    Correcion of signs to display

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const {DivideByZeroError} = require('./divide-by-zero-error');
 const calculate = (operator, a, b) => {
     switch (operator) {
         case '+': return `${a} + ${b} = ${a + b}`;
-        case '-': return `${a} + ${b} = ${a - b}`;
-        case '*': return `${a} + ${b} = ${a * b}`;
+        case '-': return `${a} - ${b} = ${a - b}`;
+        case '*': return `${a} * ${b} = ${a * b}`;
         case '/': 
             if (b !== 0) {
-              return `${a} + ${b} = ${a / b}`
+              return `${a} / ${b} = ${a / b}`
             }
             else {
               throw new DivideByZeroError();
@@ -24,4 +24,8 @@ const calculate = (operator, a, b) => {
 // console.log(calculate('/', 2, 4));
 // console.log(calculate('/', 2, 0));
 
+console.log(calculate('+', 4, 3));
+console.log(calculate('-', 4, 3));
+console.log(calculate('*', 4, 3));
+console.log(calculate('/', 4, 3));
 console.log(calculate('/', 4, 0));
